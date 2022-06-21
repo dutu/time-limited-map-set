59bf5db8728de520b5e449b17190e98ae7115e80
dutu <6230798+dutu@users.noreply.github.com>
2022-06-21 13:25:42 +0300

    first version

---
+++ index.mjs
@@ -0,0 +1,68 @@
+export class TimeLimitedMap extends Map {
+  constructor(maxMs) {
+    super()
+    this._mts = new Map()
+    this.maxMs = maxMs
+    Object.getOwnPropertyNames(Object.getPrototypeOf(new Map())).forEach((prop) => {
+      if (typeof super[prop] === 'function' && prop !== 'constructor') {
+        if (prop === 'set') {
+          this[prop] = (...args) => {
+            this._cleanup()
+            this._mts.set(args[0], Date.now())
+            return super[prop](...args)
+          }
+        } else {
+          this[prop] = (...args) => {
+            this._cleanup()
+            return super[prop](...args)
+          }
+        }
+      }
+    })
+  }
+
+  _cleanup() {
+    const maxAge = Date.now() - this.maxMs
+    Array.from(this._mts.entries()).forEach(([key, mts]) => {
+      if (mts < maxAge) {
+        this._mts.delete(key)
+        super.delete(key)
+      }
+    })
+  }
+}
+
+export class TimeLimitedSet extends Set {
+  constructor(maxMs) {
+    super()
+    this._mts = new Map()
+    this.maxMs = maxMs
+    Object.getOwnPropertyNames(Object.getPrototypeOf(new Set())).forEach((prop) => {
+      if (typeof super[prop] === 'function' && prop !== 'constructor') {
+        if (prop === 'add') {
+          this[prop] = (...args) => {
+            this._mts.delete(args[0])
+            this._cleanup()
+            this._mts.set(args[0], Date.now())
+            return super[prop](...args)
+          }
+        } else {
+          this[prop] = (...args) => {
+            this._cleanup()
+            return super[prop](...args)
+          }
+        }
+      }
+    })
+  }
+
+  _cleanup() {
+    const maxAge = Date.now() - this.maxMs
+    Array.from(this._mts.entries()).forEach(([key, mts]) => {
+      if (mts < maxAge) {
+        this._mts.delete(key)
+        super.delete(key)
+      }
+    })
+  }
+}
